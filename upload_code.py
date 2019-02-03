be157b90fc2a913b3b82a6916e1dc78f71def83f
Peter Stenger <peter.a.stenger@gmail.com>
2019-02-03 12:31:01 -0500

    fix

--- upload_code.py
+++ upload_code.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 from websocket import create_connection
 import json
-import subprocess
+# import subprocess
+import os
 
 """
 Prerequisites:
@@ -21,9 +22,9 @@ connection.send(json.dumps({"data":"visionDown"}))
 print("Enabling write")
 connection.send(json.dumps({"data":"systemWritable"}))
 print("Sending *.cpp & *.h files to Raspberry Pi/{}".format(directory))
-subprocess.run(["scp", "*.cpp", "*.h", "pi@{}:{}".format(ip, directory)])
+os.system("scp *.cpp *.h pi@{}:{}".format(ip, directory))
 print("Connecting to Pi & Building Code")
-subprocess.run(["ssh", "-t", "pi@{}".format(ip), "'{}'".format(command)])    
+os.system("ssh -t pi@{} '{}'".format(ip,command))
 print("Re-Enabling Camera")
 connection.send(json.dumps({"data":"visionUp"}))
 
